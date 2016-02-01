cbd7713e5263527757d6baf6faa3fadd4d0744c8
Marcos Ocon <oconmarcos97@gmail.com>
2016-02-01 13:25:01 -0300

    added attachments

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,9 @@ class Article < ActiveRecord::Base
 	validates :body, presence: true
 	before_create :set_visits_count
 
+	has_attached_file :cover, styles: {medium:"1280x720", thumb:"800x600"}
+	validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
+
 	def update_visits_count
 		self.update(visits_count: self.visits_count + 1)
 	end
